3a321614dccf8bfa869c5cd81274fadbae785f6b
amityaeva <amityaeva@WS-PC-88>
2021-10-01 10:06:33 +0300

    Add project files.

---
+++ TestWeb/Controllers/SecurityController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestWeb.Models;
+
+namespace TestWeb.Controllers
+{
+    public class SecurityController : Controller
+    {
+        // GET: Security
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Security());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string login, string password)
+        {
+            //if (login.Length == 0 && password.Length == 0) return HttpNotFound(); ;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Security view)
+        {
+
+            //if (view.Login.Length == 0 && view.Password.Length == 0) return HttpNotFound(); ;
+
+            return View(view);
+        }
+
+        [HttpGet]
+        public ActionResult Result(Security view)
+        {
+            return View(view);
+        }
+    }
+}
---
+++ TestWeb/Models/Security.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TestWeb.Models
+{
+    public class Security
+    {
+        [DisplayName("Login")]
+        public string Login { get; set; }
+        
+        [DisplayName("Password")]
+        public string Password { get; set; }
+    }
+}
