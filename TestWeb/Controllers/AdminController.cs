72e112dfa362c6ca84567813c9d058a93fba71a0
wwww.anechka@gmail.com <wwww.anechka@gmail.com>
2021-10-14 18:58:57 +0300

    Changed the top menu, made partial footer, added Admin area

---
+++ TestWeb/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        //[HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to admin parth";
+
+            return View();
+        }
+
+    }
+}
